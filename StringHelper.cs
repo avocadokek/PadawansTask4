482a44833f27bfc2d630c72269d0adf49ac33f92
avocadokek <47501889+avocadokek@users.noreply.github.com>
2019-02-16 12:01:22 +0200

    Add files via upload

--- StringHelper.cs
+++ StringHelper.cs
@@ -6,8 +6,28 @@ namespace PadawansTask4
     {
         public static int GetVowelCount(string str)
         {
-            // put your code here
+            int i;
+            int j = 0;
+            for (i = 0; i < str.Length; i++)
+            {
+                char ch = str[i];
+                {
+                    if ((ch == 'a') || (ch == 'e') || (ch == 'i') || (ch == 'o') || (ch == 'u'))
+                    {
+                        j++;
+                    }
+                }
+            }
+            return j;
             throw new NotImplementedException();
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Enter a string: \n");
+            string exp = Console.ReadLine();
+            var result = GetVowelCount(exp);
+            Console.WriteLine($"Your result is: {result} vowels");
         }
     }
 }
